676acc7c2e2d8a59b9028f9db0719338ca8512d6
Ashley Cove <ash.cove99@gmail.com>
2022-01-19 19:41:09 +0000

    Added comments to JS file, added error class with animation to CSS and add If statement to check if tip selected

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
+//Element Selector Variables
+
 const billInput = document.querySelector(".bill-input");
+const tipLabel = document.querySelector(".tip-label");
 const tipButtonContainer = document.querySelector(".tip-btns");
 const tipButtons = document.querySelectorAll(".tip-btn");
 const numOfPeople = document.querySelector("#num-people-input");
-
 const tipAmount = document.querySelector(".tip-amount-total");
 const totalAmount = document.querySelector(".total-amount");
 const calculateBtn = document.querySelector(".calculate-btn");
 const resetBtn = document.querySelector(".reset-btn");
-
 let tipPercentage;
 
+// Add class to tip button, button value to tipPercentage variable
 tipButtonContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".tip-btn");
 
@@ -21,33 +23,36 @@ tipButtonContainer.addEventListener("click", function (e) {
   tipPercentage = clicked.value;
 });
 
+// Function to calculate the amounts
 const calculate = () => {
   const total = billInput.value;
   const tip = tipPercentage;
   const people = numOfPeople.value;
 
   if (!tip) {
-    alert("Please select tip");
+    // alert("Please select tip");
+    tipLabel.classList.add("error");
     return;
   }
 
   const tipPerPerson = ((total / 100) * tip) / people;
   const totalPer = total / people + tipPerPerson;
-  console.log(tipPerPerson);
-  console.log(totalPer);
 
-  if (isNaN(tipPerPerson) || isNan(totalPer) === NaN) {
+  if (isNaN(tipPerPerson) || isNaN(totalPer)) {
     alert("Please check entered information");
     return;
   }
 
-  tipAmount.textContent = `$${Math.floor(tipPerPerson * 100) / 100}`;
+  tipAmount.textContent = `$${Math.floor((tipPerPerson * 100) / 100).toFixed(
+    2
+  )}`;
   totalAmount.textContent = `$${Math.round(totalPer * 100) / 100}`;
 
   resetBtn.classList.add("reset-btn-active");
   resetBtn.disabled = false;
 };
 
+// Function to change all values back to original state
 const reset = () => {
   billInput.value = "";
   numOfPeople.value = "";
@@ -58,6 +63,6 @@ const reset = () => {
   resetBtn.disabled = true;
 };
 
+// Add calculate & reset functions to buttons
 calculateBtn.addEventListener("click", calculate);
-
 resetBtn.addEventListener("click", reset);
